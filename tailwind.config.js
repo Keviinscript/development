43e0d306ed04376a7d6be4fd80f7b3d0fbffdefb
ikechukwu kevin-chika <101973303+Keviinscript@users.noreply.github.com>
2023-03-05 03:34:46 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    fontFamily: {
+      source: [ 'Source Sans Pro', 'sans-serif'],
+    },
+    extend: {
+      colors:{
+        'mainred': 'rgb(199, 0, 33)',
+        'maingrey': 'rgb(32, 32, 70)'
+      },
+    },
+  },
+  plugins: [],
+}
