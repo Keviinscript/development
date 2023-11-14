5db0f099889701e35d54823449c2a22e845186de
ikechukwu kevin-chika <ikechukwukevin725@gmail.com>
2023-11-14 09:33:14 +0100

    pushing p

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -1,3 +1,5 @@
 const timeline = gsap.timeline({defaults: {duration:1}});
     timeline.from('.alpeadalogosm' ,{y:'-100%', ease: 'power2.in', opacity: 0})
-            .from('.menubar', {x:'-100%', ease: 'power2.in', opacity: 0}, '<')
+            .from('.menubar', {x:'-100%', ease: 'power2.in', opacity: 0}, '<0.5')
+            .from('.herotext',{y: '100%', ease: 'expo.in', opacity: 0, duration: 0.5}, '<')
+            .from('.herobtn', {y: '100%', ease:'bounce.inOut', opacity: 0 , duration: 1.5}, 0)
